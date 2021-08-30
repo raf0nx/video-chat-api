a4291d8c4423ed26c916e5afd4765b1919ca8409
raf0nx <69021954+raf0nx@users.noreply.github.com>
2021-08-31 00:02:18 +0200

    feat: Add room fetching and login api endpoints

--- app-api/routes/auth.js
+++ app-api/routes/auth.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/login', (req, res) => {
+	const user = req.body;
+
+	if (!user.room || !user.username) {
+		return res.status(422).send({
+			message:
+				'Unprocessable entity! At least one required param is not provided!',
+		});
+	}
+
+	return res.status(200).send({
+		room: user.room,
+		username: user.username,
+	});
+});
+
 module.exports = router;
