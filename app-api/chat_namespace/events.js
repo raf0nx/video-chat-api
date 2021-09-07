daa0cf9d03d28b9d299059652d887f23ba46cded
raf0nx <69021954+raf0nx@users.noreply.github.com>
2021-09-07 22:36:37 +0200

    fix: Socket events didn't register properly

--- app-api/chat_namespace/events.js
+++ app-api/chat_namespace/events.js
@@ -1,19 +1,23 @@
 const users = {
-	general: [],
-	sports: [],
-	games: [],
+	GENERAL: [],
+	OFFICE: [],
+	EXPERIMENTAL: [],
 };
 let userRoom, userName;
 
 const joinRoom =
-	(socket, namespace) =>
-	({ username, room }) => {
-		socket.join(room, () => {
-			userRoom = room;
-			userName = username;
-			users[room].push({ username, privateChat: false });
-			namespace.in(room).emit('newUser', users[room]);
+	(sockeT, namespace) =>
+	({ socket }) => {
+		sockeT.join(socket.room);
+		userRoom = socket.room;
+		userName = socket.username;
+
+		users[socket.room].push({
+			username: socket.username,
+			privateChat: false,
 		});
+
+		namespace.in(socket.room).emit('newUser', users[socket.room]);
 	};
 
 const publicMessage =
@@ -25,59 +29,55 @@ const publicMessage =
 const leaveRoom =
 	(socket, namespace) =>
 	({ room, username }) => {
-		socket.leave(room, () => {
-			namespace.in(room).emit('newUser', { users, username });
-		});
+		socket.leave(room);
+		namespace.in(room).emit('newUser', { users, username });
 	};
 
 const leaveChat =
 	(socket, namespace) =>
 	({ room, username }) => {
-		socket.leave(room, () => {
-			namespace.in(room).emit('leaveChat', {
-				users,
-				message: `${username} left the room`,
-			});
+		socket.leave(room);
+		namespace.in(room).emit('leaveChat', {
+			users,
+			message: `${username} left the room`,
 		});
 	};
 
 const joinPrivateRoom =
 	(socket, namespace) =>
 	({ username, room, to, from, joinConfirm }) => {
-		socket.join(to, () => {
-			if (!room) return;
+		socket.join(to);
+		if (!room) return;
 
-			const privateChat = null;
+		const privateChat = null;
 
-			if (!!privateChat && privateChat !== username) {
-				namespace.to(to).emit('leavePrivateRoom', {
-					to,
-					room,
-					privateMessage: `${to} is already talking`,
-					from: username,
-				});
+		if (!!privateChat && privateChat !== username) {
+			namespace.to(to).emit('leavePrivateRoom', {
+				to,
+				room,
+				privateMessage: `${to} is already talking`,
+				from: username,
+			});
 
-				socket.leave(to);
-				return;
-			}
+			socket.leave(to);
+			return;
+		}
 
-			if (!joinConfirm) {
-				namespace
-					.in(room)
-					.emit('privateChat', { username, to, room, from });
-			}
-		});
+		if (!joinConfirm) {
+			namespace
+				.in(room)
+				.emit('privateChat', { username, to, room, from });
+		}
 	};
 
 const leavePrivateRoom =
 	(socket, namespace) =>
 	({ room, from, to }) => {
-		socket.leave(to, () => {
-			namespace.to(to).emit('leavePrivateRoom', {
-				to,
-				from,
-				privateMessage: `${from} has closed the chat`,
-			});
+		socket.leave(to);
+		namespace.to(to).emit('leavePrivateRoom', {
+			to,
+			from,
+			privateMessage: `${from} has closed the chat`,
 		});
 	};
 
