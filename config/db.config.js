a3b187ba896352ddb553596af5e465585910333b
raf0nx <rafalpawlowski2011@gmail.com>
2021-09-23 23:49:42 +0200

    feat: User model and first database migration

--- config/db.config.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("video_chat", "root", "Gracz1609", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
